357375f1cc5a16c98f930c8db00313302512a695
Truong Danh <37997631+thanhdanh98@users.noreply.github.com>
2019-12-17 15:33:14 +0700

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('attendantchecking1612369 app is running!');
+    expect(page.getTitleText()).toEqual('thanhdanh98 app is running!');
   });
 
   afterEach(async () => {
