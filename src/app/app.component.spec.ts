e9a6aea4dba7a357110a9953cb699225a308c0a8
Truong Danh <37997631+thanhdanh98@users.noreply.github.com>
2019-12-17 20:00:53 +0700

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'thanhdanh98'`, () => {
+  it(`should have as title 'attendantchecking1612369'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('thanhdanh98');
+    expect(app.title).toEqual('attendantchecking1612369');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('thanhdanh98 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('attendantchecking1612369 app is running!');
   });
 });
